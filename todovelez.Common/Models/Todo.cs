2557d835439926a195e90015d0663a9cc971718c
BRAYAN ANDRES VELEZ LOPEZ <brayanvelez260005@correo.itm.edu.co>
2021-08-29 10:04:23 -0500

    Add entity

---
+++ todovelez.Common/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace todovelez.Common.Models
+{
+    public class Todo
+    {
+        public DateTime dateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ todovelez.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todovelez.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime dateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
